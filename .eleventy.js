92f842798469aa8ed537927d366fe41ad02d8c6f
Gene <genemars@users.noreply.github.com>
2022-02-23 16:42:52 +0100

    Fixed =)

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/css');
   eleventyConfig.addPassthroughCopy('posts/images');
-  eleventyConfig.setDataDeepMerge(false);
   
   return {
     passthroughFileCopy: true
